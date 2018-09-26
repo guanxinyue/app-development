f3d68a0e564e1b5164b33295512085a3e8cc2664
KIDkid1412 <778799700@qq.com>
2018-09-26 23:13:43 +0800

    作业1

---
+++ app/components/parent/parent.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+
+export class ParentComponent implements OnInit {
+
+  constructor() { }
+  data:string[]=[];
+  data1:string[]=[];
+  changex(txt){
+    this.data.push(txt);
+  }
+  ngOnInit() {
+  }
+  
+}
